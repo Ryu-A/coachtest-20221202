d92c32d63f58ce9dad597118703ae12ed2425070
Ryu-A <ryuichiro.annoura@gmail.com>
2022-12-02 17:43:42 +0900

    first-commit

---
+++ course.php
@@ -0,0 +1,29 @@
+<?php
+$txt = "PHP";
+$txt = "Laravel";
+echo $txt;
+// define関数
+define("FELANGUAGE", "Vue.js");
+print FELANGUAGE;
+echo "<br />";
+
+$name1 = "Sato" . "Taro";
+$name2 =  "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name1;
+echo "<br />";
+echo $name2 . "Jiro";
+echo "<br />";
+echo $last_name . $first_name;
+
+$a = 10;
+$b = 100;
+$c = -20;
+
+var_dump($a);
+echo "<br>";
+var_dump($b);
+echo "<br>";
+var_dump($c);
